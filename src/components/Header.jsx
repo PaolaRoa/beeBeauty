8b3f98f194c7818321c6565e7276227676fce5a6
pcProgramate <paolaroa1925@gmail.com>
2021-02-22 17:03:31 -0500

    add rrss icons on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import wa from '../images/wa.png';
+import ig from '../images/ig.png'
+import bb from '../images/beeLogo.png'
 
 const Header = () => {
     return ( 
         <header>
-            <img src="https://scontent-bog1-1.cdninstagram.com/v/t51.2885-19/s320x320/102723614_828503660891754_1581765600553203555_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_ohc=xEeEr-kltWIAX-JOPUt&tp=1&oh=43178d1352de3e5402237b222e8eae36&oe=605F178B" alt="logo beeBeauty" className="logoSmall"/>
+            <img src={bb} alt="logo beeBeauty" className="logoSmall"/>
             <div>
-                
+                <img src={wa} alt="logo whatsapp" className="rrss"/>
+                <img src={ig} alt="logo instagram " className="rrss"/>
+
             </div>
 
         </header>
