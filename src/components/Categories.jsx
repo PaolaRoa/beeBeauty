5bcc3233dfa73cd768e136eb0c629adac2833cd8
pcProgramate <paolaroa1925@gmail.com>
2021-03-04 16:45:16 -0500

    fix categorie button

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,18 +4,27 @@ import '../css/categories.css'
 
 
 const Categories = (props) => {
-
+    
+    const setBtn =(element)=>{
+        let l = element.parentNode.parentNode.childNodes;
+        l.forEach(
+            list=> {list.classList.remove('active');
+            // console.log(list)
+            })
+            console.log( element.parentNode)
+            element.parentNode.classList.add('active')
+    }
     return ( 
         <>
         <h3 id="cat">Categorias</h3>
         <div className="catContainer">
             <ul>
                 {/* this button shows all products setting the products state with products array */}
-                <li><button type="button" className='catBtn' onClick = {(e)=>{props.catSelector(products)}}>Todo</button></li>
+                <li className = 'active'><button type="button" className='' onClick = {(e)=>{props.catSelector(products);setBtn(e.target)}}>Todo</button></li>
                 {/* maps the products array and generates a button for every categorie */}
                 {products.map(categorie =>(
                     <li key= {products.indexOf(categorie)} >
-                        <button type="button" className='catBtn' onClick={(e)=>{props.catSelector(categorie.items);}}>{categorie.cname}</button>
+                        <button type="button" className='' onClick={(e)=>{props.catSelector(categorie.items);setBtn(e.target)}}>{categorie.cname}</button>
                     </li>
                 )
                 )}
